d3e7cd73a358bf57d4fd0a63adc2e8f3d459e763
Thao Nguyen <102851421+thaont2404@users.noreply.github.com>
2022-05-21 13:12:22 +0700

    Update hangman.py
    Additional:
    1. Better visual board to display results after each input.
    2. Do not allow users to input numbers, > 1 letter as well as catch errors.

--- hangman.py
+++ hangman.py
@@ -1,29 +1,5 @@
 import random
 
-# hangman words to guess
-hangman_list = [
-    " abruptly ", " absurd ", " abyss ", " affix ", " askew ", " avenue ", " awkward ", " axiom ", " azure ", " bagpipes ", " bandwagon ", " banjo ",
-    " bayou ", " beekeeper ", " bikini ", " blitz ", " blizzard ", " boggle ", " bookworm ", " boxcar ", " boxful ", " buckaroo ", " buffalo ",
-    " buffoon ", " buxom ", " buzzard ", " buzzing ", " buzzwords ", " caliph ", " cobweb ", " cockiness ", " croquet ", " crypt ", " curacao ",
-    " cycle ", " daiquiri ", " dirndl ", " disavow ", " dizzying ", " duplex ", " dwarves ", " embezzle ", " equip ", " espionage ", " euouae ",
-    " exodus ", " faking ", " fishhook ", " fixable ", " fjord ", " flapjack ", " flopping ", " fluffiness ", " flyby ", " foxglove ", " frazzled ",
-    " frizzled ", " fuchsia ", " funny ", " gabby ", " galaxy ", " galvanize ", " gazebo ", " giaour ", " gizmo ", " glowworm ", " glyph ",
-    " gnarly ", " gnostic ", " gossip ", " grogginess ", " haiku ", " haphazard ", " hyphen ", " iatrogenic ", " icebox ", " injury ", " ivory ",
-    " ivy ", " jackpot ", " jaundice ", " jawbreaker ", " jaywalk ", " jazziest ", " jazzy ", " jelly ", " jigsaw ", " jinx ", " jiujitsu ",
-    " jockey ", " jogging ", " joking ", " jovial ", " joyful ", " juicy ", " jukebox ", " jumbo ", " kayak ", " kazoo ", " keyhole ", " khaki ",
-    " kilobyte ", " kiosk ", " kitsch ", " kiwifruit ", " klutz ", " knapsack ", " larynx ", " lengths ", " lucky ", " luxury ", " lymph ",
-    " marquis ", " matrix ", " megahertz ", " microwave ", " mnemonic ", " mystify ", " naphtha ", " nightclub ", " nowadays ", " numbskull ",
-    " nymph ", " onyx ", " ovary ", " oxidize ", " oxygen ", " pajama ", " peekaboo ", " phlegm ", " pixel ", " pizazz ", " pneumonia ", " polka ",
-    " pshaw ", " psyche ", " puppy ", " puzzling ", " quartz ", " queue ", " quips ", " quixotic ", " quiz ", " quizzes ", " quorum ", " razzmatazz ",
-    " rhubarb ", " rhythm ", " rickshaw ", " schnapps ", " scratch ", " shiv ", " snazzy ", " sphinx ", " spritz ", " squawk ", " staff ",
-    " strength ", " strengths ", " stretch ", " stronghold ", " stymied ", " subway ", " swivel ", " syndrome ", " thriftless ", " thumbscrew ",
-    " topaz ", " transcript ", " transgress ", " transplant ", " triphthong ", " twelfth ", " twelfths ", " unknown ", " unworthy ", " unzip ",
-    " uptown ", " vaporize ", " vixen ", " vodka ", " voodoo ", " vortex ", " voyeurism ", " walkway ", " waltz ", " wave ", " wavy ", " waxy ",
-    " wellspring ", " wheezy ", " whiskey ", " whizzing ", " whomever ", " wimpy ", " witchcraft ", " wizard ", " woozy ", " wristwatch ", " wyvern ",
-    " xylophone ", " yachtsman ", " yippee ", " yoked ", " youthful ", " yummy ", " zephyr ", " zigzag ", " zigzagging ", " zilch ", " zipper ",
-    " zodiac ", " zombie ",
-]
-
 hangmanresults = [''''
   +---+
   |   |
@@ -75,27 +51,35 @@ hangmanresults = [''''
       |
 =========''']
 
-# the secret word
-hangman = random.choice(hangman_list).strip()
-hangman_word = []
-# input the hangman word in a list -> for each correct guess, remove the word from hangman list and put it into the human correct
-for i in hangman:
-    hangman_word.append(i)
-
 # the user guess
 human_incorrect = []
 human_correct = []
 visual_board = []
 human_incorrect_count = 0
 
-# game start
-print('\nWELCOME TO THE HANGMAN GAME (((o(*ﾟ▽ﾟ*)o)))')
+# hangman words to guess
+hangman_dictionary = {'dogs': 'animals', 'cats': 'animals', 'keys': 'objects'}
+hangman_list = list(hangman_dictionary.keys())
+
+# the secret word
+hangman = random.choice(hangman_list)
+
+# input the hangman word in a list -> for each correct guess, remove the word from hangman list and put it into the human correct
+hangman_word = []
+for i in hangman:
+    hangman_word.append(i)
+    
+word_to_guess = hangman_word[:]
+# game start -> print hint (eg. This word belongs to which topic)
+print(f'\nWELCOME TO THE HANGMAN GAME (((o(*ﾟ▽ﾟ*)o)))\nCAN YOU FIND THE WORD BEFORE THE HANGMAN HANGS '
+      f'YOU?!\n----------------------------------------------------'
+      f'\nThis words belongs to the topic: {hangman_dictionary[hangman].upper()}')
 
 # visual: place to input word (how many word) --> eg. two => print _ _ _ => append _ _ _ into visual_board
 count_word = len(hangman)
 visual_empty = '_ '
-print(f"The word contain {count_word} letters")
 x = 0
+print(f"The word contain {count_word} letters")
 while x < count_word:
     visual_board.append(visual_empty)
     print(visual_empty, end='')
@@ -105,11 +89,23 @@ while True:
     # criterial for game active -> human hasn't made 7 incorrect guesses
     if human_incorrect_count < 7:
         # not win (still word not have been guess)
-        if hangman_word:
-            
+        if word_to_guess:
             # input guess
             human_guess = input('\n\nInput your guess here: ').lower()
-            
+    
+            # input > 1 letter or number -> not allowed (try - execept, use continue to input again)
+            try:
+                int(human_guess)
+            except ValueError:
+                if len(human_guess) > 1:
+                    print('Only input one characters!')
+                    continue
+                else:
+                    pass
+            else:
+                print('Only input characters! Number not allowed')
+                continue
+                
             # already guess -> rule out
             if human_guess in human_incorrect or human_guess in human_correct:
                 print(f'You have already guess {human_guess}')
@@ -117,7 +113,7 @@ while True:
                 print('\nYour guess: ', end='')
                 for w in human_correct:
                     print(w.upper(), end=' ')
-                    
+            
             # acceptable guess -> correct or incorrect
             else:
                 # incorrect -> hangman
@@ -133,20 +129,26 @@ while True:
                     count_guess = hangman_word.count(human_guess)
                     print(f'\nCorrect! There is {count_guess} {human_guess} in the word!')
                     
-                    # visual board
-                    index_guess = hangman.index(human_guess)
-                    del visual_board[index_guess]
-                    visual_board.insert(index_guess, human_guess)
+                    # visual board -> start with a board contain all correct word.
+                    # Using an if loop: If the guess is correct -> pass. If wrong, print '_' instead of the correct word
+                    visual_board = hangman_word[:]
+                    for check in visual_board:
+                        if check != human_guess:
+                            if check not in human_correct:
+                                index_guess = visual_board.index(check)
+                                del visual_board[index_guess]
+                                visual_board.insert(index_guess, '_')
                     for v in visual_board:
                         print(v.upper(), end=' ')
                     
-                    # remove the word from hangman list -> put it into the human correct
-                    hangman_word.remove(human_guess)
+                    # put the guess into the human correct, remove the word from the word_to_guess
                     human_correct.append(human_guess)
+                    word_to_guess.remove(human_guess)
                     
+        
         else:
             print('\n\n----------------------------------------\n'
-                  'Congratulate! YOU WIN O(≧∇≦o)')
+                  'CONGRATULATIONS! YOU WIN O(≧∇≦o)')
             break
     
     # Game not active -> human made 7 mistakes
@@ -154,6 +156,6 @@ while True:
         print('\n\n----------------------------------------\n'
               'GAME OVER\nYOU LOSE.')
         break
-        
+
 # show results
 print(f'\n> The secret word is: {hangman.upper()}')
